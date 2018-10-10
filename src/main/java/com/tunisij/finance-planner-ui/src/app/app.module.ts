572d186701438f86686e856cbd853837bddba403
John Tunisi <tunisij@gmail.com>
2018-10-09 20:20:40 -0400

    added create user page + bootstrap

--- src/main/java/com/tunisij/finance-planner-ui/src/app/app.module.ts
+++ src/main/java/com/tunisij/finance-planner-ui/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SalaryInfoComponent } from './salary-info/salary-info.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import {AccordionModule, ButtonModule, InputTextModule, SidebarModule} from 'primeng/primeng';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SalaryInfoComponent
+    SalaryInfoComponent,
+    CreateUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    InputTextModule,
+    ButtonModule,
+    AccordionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
